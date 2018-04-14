67620f695be3a5e9ca3c7abb0a717b06981287b1
Chris Kavanagh <ckava3@gmail.com>
2018-04-14 05:13:56 -0400

    note

--- ng6/src/app/interceptor.ts
+++ ng6/src/app/interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { LoginService } from './services/login.service';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
